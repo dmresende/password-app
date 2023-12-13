9e9ffd900871823c9dfbc2cc7e1c821963695b17
Douglas Resende <douglasmresende@gmail.com>
2023-12-13 09:20:49 -0300

    feat: gerador de senha

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import Slider from '@react-native-community/slider';
 
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()_-+=^[]{}'
+
 export default function App() {
+  const [size, setSize] = useState(10);
+  const [password, setPasswordValue] = useState('')
+
+  function generatorPassword() {
+    let password = '';
+    // busca caracter aleatorio
+    for (let i = 0, n = charset.length; i < size; i++) {
+      // charAt pega caracater
+      // Math.floor pega valor inteiro
+      //Math.random pega valor aleatorio
+      password += charset.charAt(Math.floor(Math.random() * n))
+    }
+    //muda status
+    setPasswordValue(password);
+  }
+
   return (
     <View style={styles.container}>
-      <Image  style={styles.logo} source={require("./src/assets/logo.png")}/>
+      <Image style={styles.logo} source={require("./src/assets/logo.png")} />
       <StatusBar style="auto" />
 
-      <Text style={styles.title}>20 caracteres</Text>
+      <Text style={styles.title}>{size} caracteres</Text>
 
+      {/* Uma biblioteca de componentes React Native que fornece acesso à IU do sistema para um controle deslizante. */}
       <View style={styles.area}>
-        {/* componente intalado e importado */}
         <Slider
           style={styles.areaSlider}
           minimumValue={6}
@@ -19,9 +38,17 @@ export default function App() {
           maximumTrackTintColor='#000'
           minimumTrackTintColor='#33CC66'
           thumbTintColor='#392de9'
+          value={size}
+
+          // Propriedade slider que permite fazer a troca do valor
+          onValueChange={(value) => setSize(value.toFixed(0))}
+
         />
       </View>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={generatorPassword}
+      >
         <Text style={styles.buttonText}>
           Gerar senha
         </Text>
@@ -37,36 +64,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo:{
+  logo: {
     marginBottom: 60,
   },
-  title:{
-    fontSize:30,
+  title: {
+    fontSize: 30,
     fontWeight: 'bold',
   },
-  area:{
+  area: {
     marginTop: 14,
     marginBottom: 14,
     width: "80%",
     backgroundColor: '#FFF',
-    borderRadius:8,
-    padding:6,
-    
+    borderRadius: 8,
+    padding: 6,
+
   },
-  areaSlider:{
+  areaSlider: {
     height: 50,
   },
-  button:{
-    backgroundColor:'#392de9',
-    width:"80%",
-    height:50,
-    alignItems:"center",
+  button: {
+    backgroundColor: '#392de9',
+    width: "80%",
+    height: 50,
+    alignItems: "center",
     justifyContent: "center",
-    borderRadius:8,
-    marginBottom:18,
+    borderRadius: 8,
+    marginBottom: 18,
   },
-  buttonText:{
+  buttonText: {
     color: '#FFF',
-    fontSize:20,
+    fontSize: 20,
   },
 });
